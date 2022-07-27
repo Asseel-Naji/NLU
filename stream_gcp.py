f5078c0dc1c21d582c4c2a83a3df5c5f15732591
A. Naji <asseel.naji96@gmail.com>
2022-07-27 21:38:32 +0300

    Fixing the classes in stream_gcp.py, calling them from main.py

--- stream_gcp.py
+++ stream_gcp.py
@@ -27,6 +27,7 @@ Example usage:
 
 # [START speech_transcribe_streaming_mic]
 from __future__ import division
+from msvcrt import kbhit
 
 import os
 import re
@@ -36,8 +37,6 @@ import pyaudio
 from google.cloud import speech
 from six.moves import queue
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./secrets/gkey.json"
-GOOGLE_APPLICATION_CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
 # Audio recording parameters
 RATE = 16000
@@ -112,94 +111,81 @@ class MicrophoneStream(object):
             yield b"".join(data)
 
 
-def listen_print_loop(responses):
-    """Iterates through server responses and prints them.
-
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-    """
-    num_chars_printed = 0
-    for response in responses:
-        if not response.results:
-            continue
-
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
-        result = response.results[0]
-        if not result.alternatives:
-            continue
-
-        # Display the transcription of the top alternative.
-        transcript = result.alternatives[0].transcript
-
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
-        overwrite_chars = " " * (num_chars_printed - len(transcript))
-
-        if not result.is_final:
-            # win.label.setText((transcript + overwrite_chars + "\r"))
-            with open("./random_tests/transcript.txt", "w") as f:
-                f.write(transcript)
-            with open("./random_tests/confidence.txt", "a") as f:
-                f.write(str(result.alternatives[0].confidence)) #wut happened here in the previous commti I have no idea
-
-            sys.stdout.write(transcript + overwrite_chars + "\r")
-            sys.stdout.flush()
-
-            num_chars_printed = len(transcript)
-
-        else:
-            print(transcript + overwrite_chars)
-
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            if re.search(r"\b(exit|quit|وقف تسجيل)\b", transcript, re.I):
-                print("Exiting..")
-                break
-
-            num_chars_printed = 0
-
-
-def main():
-    language_code = "ar-JO"  # a BCP-47 language tag
-    client = speech.SpeechClient()
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=RATE,
-        language_code=language_code,
-        enable_word_confidence=True,
-    )
-
-    streaming_config = speech.StreamingRecognitionConfig(
-        config=config, interim_results=True
-    )
-
-    with MicrophoneStream(RATE, CHUNK) as stream:
-        audio_generator = stream.generator()
-        print("Please start talking")
-
-        requests = (
-            speech.StreamingRecognizeRequest(audio_content=content)
-            for content in audio_generator
+class StartTalking:
+    def __init__(self, language_code="ar-JO", enable_word_confidence=True):
+        client = speech.SpeechClient()
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=RATE,
+            language_code=language_code,
+            enable_word_confidence=enable_word_confidence,
         )
 
-        responses = client.streaming_recognize(streaming_config, requests)
-        # Now, put the transcription responses to use.
-        listen_print_loop(responses)
+        streaming_config = speech.StreamingRecognitionConfig(
+            config=config, interim_results=True
+        )
+
+        with MicrophoneStream(RATE, CHUNK) as stream:
+            audio_generator = stream.generator()
+            print("Please start talking")
+
+            requests = (
+                speech.StreamingRecognizeRequest(audio_content=content)
+                for content in audio_generator
+            )
+
+            responses = client.streaming_recognize(streaming_config, requests)
+            # Now, put the transcription responses to use.
+            self.listen_print_loop(responses)
+
+    def listen_print_loop(responses):
+        num_chars_printed = 0
+        for response in responses:
+            if not response.results:
+                continue
+
+            # The `results` list is consecutive. For streaming, we only care about
+            # the first result being considered, since once it's `is_final`, it
+            # moves on to considering the next utterance.
+            result = response.results[0]
+            if not result.alternatives:
+                continue
+
+            # Display the transcription of the top alternative.
+            transcript = result.alternatives[0].transcript
+
+            # Display interim results, but with a carriage return at the end of the
+            # line, so subsequent lines will overwrite them.
+            #
+            # If the previous result was longer than this one, we need to print
+            # some extra spaces to overwrite the previous result
+            overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+            if not result.is_final:
+                # win.label.setText((transcript + overwrite_chars + "\r"))
+                with open("./random_tests/transcript.txt", "w") as f:
+                    f.write(transcript)
+                with open("./random_tests/confidence.txt", "a") as f:
+                    f.write(
+                        str(result.alternatives[0].confidence)
+                    )  # wut happened here in the previous commti I have no idea
+
+                sys.stdout.write(transcript + overwrite_chars + "\r")
+                sys.stdout.flush()
+
+                num_chars_printed = len(transcript)
+
+            else:
+                print(transcript + overwrite_chars)
+
+                # Exit recognition if any of the transcribed phrases could be
+                # one of our keywords.
+                if re.search(r"\b(exit|quit|وقف تسجيل)\b", transcript, re.I):
+                    print("Exiting..")
+                    break
+
+                num_chars_printed = 0
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
